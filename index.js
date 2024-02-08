8e8bc130b605a63358f388111484c15e5c4cc37f
Josh Dickerson <dickersonj87@gmail.com>
2024-02-08 13:49:09 -0600

    fixed checkboxes and removed some elements

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ $(document).ready(function(){
             console.log('Task object:', response.tasks[0]);
             $('#todo-list').empty();
             response.tasks.forEach(function (task) {
-                var status = task.completed ? 'Finished' : 'Not Finished';
-                var activeTasks = JSON.parse(localStorage.getItem('activeTasks')) || [];
-                var checked = task.active ? 'checked' : '';
-                $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><p class="col-xs-2">' + status + '</p><button class="delete" data-id="' + task.id + '">Delete</button><button class="change-status" data-id="' + task.id + '">Done!</button><input type="checkbox" class="set-active" data-id="' + task.id + '" ' + checked + '></div>');
+              $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
             })
           },
           error: function (request, textStatus, errorMessage) {
@@ -65,14 +62,12 @@ $(document).ready(function(){
         deleteTask(id);
       });
 
-      var changeStatus = function (id) {
+      var markTaskComplete = function (id) {
         $.ajax({
           type: 'PUT',
           url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=1163',
-          contentType: 'application/json',
           dataType: 'json',
           success: function (response, textStatus) {
-            console.log('PUT request response:', response);
             getAndDisplayAllTasks();
           },
           error: function (request, textStatus, errorMessage) {
@@ -80,26 +75,27 @@ $(document).ready(function(){
           }
         });
       }
-      
-      $(document).on('click', '.change-status', function () {
-        var id = $(this).data('id');
-        changeStatus(id);
-      });
-
-      var setActive = function (id, checked) {
-        var activeTasks = JSON.parse(localStorage.getItem('activeTasks')) || [];
-        if (checked) {
-          activeTasks.push(id);
-        } else {
-          var index = activeTasks.indexOf(id);
-          if (index !== -1) activeTasks.splice(index, 1);
-        }
-        localStorage.setItem('activeTasks', JSON.stringify(activeTasks));
+    
+      var markTaskActive = function (id) {
+        $.ajax({
+          type: 'PUT',
+          url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=1163',
+          dataType: 'json',
+          success: function (response, textStatus) {
+            getAndDisplayAllTasks();
+          },
+          error: function (request, textStatus, errorMessage) {
+            console.log(errorMessage);
+          }
+        });
       }
-
-      $(document).on('change', '.set-active', function () {
-        var id = $(this).data('id');
-        setActive(id, this.checked);
+    
+      $(document).on('change', '.mark-complete', function () {
+        if (this.checked) {
+          markTaskComplete($(this).data('id'));
+        } else {
+          markTaskActive($(this).data('id'));
+        }
       });
       
       getAndDisplayAllTasks();
